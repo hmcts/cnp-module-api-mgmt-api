8cee80afe1066d3addf3537ac08e7733ab65d1fd
Enda Kelly <enda.kelly@hmcts.net>
2021-06-10 09:46:26 +0100

    Removed condition on mgmt product api

--- link-to-product.tf
+++ link-to-product.tf
@@ -4,6 +4,4 @@ resource "azurerm_api_management_product_api" "link_to_product" {
   resource_group_name = "${var.api_mgmt_rg}"
 
   product_id = "${var.product_id}"
-
-  count = "${var.product_id != "" ? 1 : 0}"
 }
